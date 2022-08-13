01ccdf2609ce6e0867f9737267b8b3db754d4e8a
Recep Darıcı <recepdarici4507@gmail.com>
2022-08-13 23:37:02 +0300

    java son

--- src/day47_maps/C04_Entry.java
+++ src/day47_maps/C04_Entry.java
@@ -9,6 +9,6 @@ public class C04_Entry {
     public static void main(String[] args) {
         Map<Integer,String> sinifListMap= ReusableMethods.mapOlustur();
         System.out.println(sinifListMap);
-        Set<Map.Entry<Integer,String>> sinifListMap.entrySet()=sinifListMap.entrySet();
+
     }
 }
---
+++ src/day48_maps_TheEnd/C03_Compute.java
@@ -0,0 +1,11 @@
+package day48_maps_TheEnd;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class C03_Compute {
+    public static void main(String[] args) {
+        Map<String,Integer> harfMapi=new HashMap<>();
+        harfMapi.put("A",3);
+    }
+}
